18a46013ff1ad27ae45fe0ab008bf8a475b6601a
Curtis <curtis@lagraff.me>
2016-02-02 20:29:00 -0500

    Making changes

--- main.cpp
+++ main.cpp
@@ -5,80 +5,45 @@
 const int ICE_REV_LIMIT = 13000;
 
 AnalogIn att(p19);  // (Input) Attenuator signal
-AnalogIn tps(p20);  // (Input) Throttle Position Sensor signal
 AnalogOut yasa(p18); // (Output) YASA MC voltage
 Serial mc(p28, p27); // (Input/Output) Motor Controller serial line
+PwmOut mc2(p21);    // (Output) PWM line to new Motor Controller
 
 CAN vnet(p9, p10); // (Input/Output) Vehicle CAN network lines
 CANMessage cmsg; // Stores most recent, highest priority CAN message.
 
 
 int main() {
+    char counter = 42;
     int ice_rev = 0;
+    float waitDelay = 0.01;
     
-    ThrottleBodyCtrl tbCtrl(&mc, &tps);
-    tbCtrl.idle();
+    mc2.period_ms(20);
     
-    float waitDelay = 0.00001;
-
     while (1) {
         if (vnet.read(cmsg)) {
-            if (cmsg.id == 1) { // Change to appropraite CAN message ID
-                // convert CAN message to int RPM value
-                // ice_rev = can message contents
+            if (cmsg.id == 1337) {
+                if (cmsg.data[0] == 42) {
+                    utils::set_leds(true, true, true, true);
+                }
             }
         }
         
         if (ice_rev >= ICE_REV_LIMIT) {
-            //tbCtrl.idle();
             yasa = 0;
             continue;
         }
         
         // Set the Yasa MC to match the attenuator
         yasa = att;
-        
-        
-        float tps = tbCtrl.percent();
-
-        if ((att - 0.07f) < tps && tps < (att + 0.07f)) { // hold
-            tbCtrl.increaseAmt = tbCtrl.defaultIncreaseAmount;
-            tbCtrl.decreaseAmt = tbCtrl.defaultDecreaseAmount;
-            
-            if (tps > 0.80f) {
-                mc.putc(0x85); // Set motor to forward diretion
-                mc.putc(0x00);
-                mc.putc(0x25);
-            } else {
-                mc.putc(0x85); // Set motor to forward diretion
-                mc.putc(0x00);
-                mc.putc(0x10); 
-            }
-            
-            wait(waitDelay); 
-        } else if (att < tps) { // decrease
-            tbCtrl.decrease();
-            
-            tbCtrl.decreaseAmt -= 0x01;
-            if (tbCtrl.decreaseAmt < 0x00) {
-                 tbCtrl.decreaseAmt = 0x00;
-            }
-            
-            wait(waitDelay);                
+        int target = int(att * 2000.00 + 300);
+        if (target > 2500) {
+            target = 2500;
+        } else if (target < 0) {
+            target = 0;
         }
-        else if (att > tps) { // increase
-            tbCtrl.increase();
             
-            if (att - tps > 0.50f) {
-                tbCtrl.increaseAmt += 0x03;
-            } else {
-                tbCtrl.increaseAmt += 0x01;
-            }
-            if (tbCtrl.increaseAmt > 0x25) {
-                tbCtrl.increaseAmt = 0x25;
-            }
-            
-            wait(waitDelay);
-        }
+        mc2.pulsewidth_us(target);
+        wait(waitDelay);
     }
 }
